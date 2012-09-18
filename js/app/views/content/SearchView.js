be779602faca17a9b6e065d809b90c9b58635664
Rodrigo Duarte Sousa <rodrigodsousa@gmail.com>
2012-09-18 14:41:08 -0300

    explore page

---
+++ js/app/views/content/SearchView.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2012 Denis Washington <denisw@online.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+define(function(require) {
+  var Backbone = require('backbone');
+  var template = require('text!templates/discover.html')
+
+  var DiscoverView = Backbone.View.extend({
+    className: 'discoverChannels clearfix',
+
+    render: function() {
+      this.$el.html(_.template(template));
+    }
+  });
+
+  return DiscoverView;
+});
