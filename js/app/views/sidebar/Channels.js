91f5471552ac693ba3ebd99af7c1f9d7d1192fd3
Rodrigo Duarte Sousa <rodrigodsousa@gmail.com>
2012-09-19 09:12:17 -0300

    SidebarPage

---
+++ js/app/views/sidebar/Channels.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2012 Denis Washington <denisw@online.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+define(function(require) {
+  var avatarFallback = require('util/avatarFallback');
+  var Backbone = require('backbone');
+  var template = require('text!templates/sidebar/channels.html')
+
+  var Channels = Backbone.View.extend({
+    className: 'channels',
+
+    render: function() {
+      this.$el.html(_.template(template, {channels: this.model.channels}));
+    }
+  });
+
+  return Channels;
+});
