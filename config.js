7f4a1340ee2687eb123a50b64fd2a9b55b7d0ab1
Rodrigo Duarte Sousa <rodrigodsousa@gmail.com>
2012-10-11 11:37:55 -0300

    set maxWidth on embedly

--- config.js
+++ config.js
@@ -2,6 +2,5 @@ define({
   baseUrl: 'https://api.buddycloud.org',
   homeDomain: 'buddycloud.org',
   defaultChannel: 'lounge@topics.buddycloud.org',
-  embedlyKey: '593c0a9858e611e188dc4040d3dc5c07',
   release: true
 });
