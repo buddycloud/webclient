2e7dbae5b81c2f3ae0c18788b7b1bde2bae8ca49
Rodrigo Duarte Sousa <rodrigodsousa@gmail.com>
2012-11-05 09:47:16 -0300

    fix loginError action

--- js/app/main.js
+++ js/app/main.js
@@ -45,8 +45,7 @@ define(function(require) {
     });
     user.on('loginError', function() {
       alert('Wrong username or password.');
-      user.credentials.set({username: null, password: null});
-      user.credentials.save();
+      user.credentials.save({username: null, password: null});
       route(user);
     });
     user.login({permanent: localStorage.loginPermanent === 'true'});
