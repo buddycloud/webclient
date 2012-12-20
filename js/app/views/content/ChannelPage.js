7ed2f7e98e1e92b196df72e94eb5b4e254a4ef71
Rodrigo Duarte Sousa <rodrigodsousa@gmail.com>
2012-12-20 09:58:55 -0300

    fixes some anon page issues

--- js/app/views/content/ChannelPage.js
+++ js/app/views/content/ChannelPage.js
@@ -28,13 +28,10 @@ define(function(require) {
         channel: this.options.channel,
         user: this.options.user
       });
-      /*this.model.bind('fetch', this._begin, this);
-      this.model.bind('error', this._error, this);
-      this.model.fetch({credentials: this.options.user.credentials});*/
       this.render();
 
       if (this.options.user.isAnonymous()) {
-        this.overlay = new AnonChannelOverlay({model: this.options.user});
+        
       }
     },
 
@@ -42,7 +39,7 @@ define(function(require) {
       this.view.render();
       var $content = $('.content');
 
-      if (this.overlay) {
+      if (this.options.user.isAnonymous()) {
         this._renderAnonPage($content);
       } else {
         $content.html(this.view.el);
@@ -51,13 +48,19 @@ define(function(require) {
     },
 
     _renderAnonPage: function(content) {
-      this.overlay.render();
+      this._renderOverlay();
+
       var $center = $('<div class="stupidFirefoxFlexBoxBug centered stretchWidth stretchHeight"></div>');
       $center.html(this.view.el);
 
       content.addClass('anonView');
       content.append(this.overlay.el);
       content.append($center);
+    },
+
+    _renderOverlay: function() {
+      this.overlay = this.overlay = new AnonChannelOverlay({model: this.options.user});
+      this.overlay.render();
     },
 
     destroy: function() {
