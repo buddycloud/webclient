a41c70d4d8fbf6b2f3c7b14b7c7f76a0b01e95ba
Severin Kistner <sekistner@gmail.com>
2013-02-24 22:15:05 +0100

    integrated dev server in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    connect: {
-      app: {
-        options: {
-          port: 8000,
-          base: ''
-        }
-      }
-    },
-
     testacular: {
       unit: {
         configFile: 'testacular.conf.js',
@@ -24,12 +15,39 @@ module.exports = function(grunt) {
   });
 
   // Plug-ins
-  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('gruntacular');
 
   // Tasks
-  grunt.registerTask('default', ['server']);
-  grunt.registerTask('server', ['connect', 'watch']);
+  grunt.registerTask('default', ['server', 'watch']);
   grunt.registerTask('test', ['testacular']);
+
+  // server
+  var express = require('express');
+  var fs = require('fs');
+  
+  grunt.registerTask('server', 'Start a custom static web server.', function() {
+
+    var mdir = __dirname;
+    var app = express();
+
+    app.use('/js', express.static(mdir + '/js'));
+    app.use('/css', express.static(mdir + '/css'));
+    app.use('/img', express.static(mdir + '/img'));
+    app.use('/locales', express.static(mdir + '/locales'));
+    app.use('/templates', express.static(mdir + '/templates'));
+    app.get('/config.js', function(req, res) {
+    fs.readFile(mdir + '/config.js', 'utf8', function(error, file) {
+        res.contentType('application/javascript');
+        res.send(file);
+      });
+    });
+    app.get('*', function(req, res) {
+    fs.readFile(mdir + '/index.html', 'utf8', function(error, file) {
+        res.send(file);
+      });
+    });
+    app.listen(3000);
+  });
+
 }
