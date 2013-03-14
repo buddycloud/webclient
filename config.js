f5b03cd8c424f76b692fb84d340f2365c635edcd
Reginald dude <simon@buddycloud.com>
2013-03-14 12:05:00 +0100

    clear settings.

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 define({
-	// if you just want to develop, set these two to buddycloud.org
+  // API address: where you API is hosted. 
+  // Note: it is not possible to use self signed certificates for AJAX requests.
+  // https://buddycloud.org/wiki/Install#SSL_and_API_requests
   baseUrl: 'https://api.EXAMPLE.COM',
+  
+  // Domain that you your users live on. For example, if your users' 
+  // IDs look like user@example.com then the homeDomain would be example.com.
   homeDomain: 'EXAMPLE.COM',
-  defaultChannel: 'lounge@topics.buddycloud.org',
+  
   // To enable Embedly, you must sign up on the http://embed.ly website.
   // If you are not using Embedly, you may see "some content is unencrypted"
   // messages while browsing, which can be solved by changing embedlySecure
