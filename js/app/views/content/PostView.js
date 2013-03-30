a017db29133a8b1adf6f33daaa6aba98412dd38b
Vítor Avelino <contact@vitoravelino.net>
2013-03-30 20:30:20 +0100

    Removing unecessary console.log

--- js/app/views/content/PostView.js
+++ js/app/views/content/PostView.js
@@ -156,7 +156,7 @@ define(function(require) {
     _actionButton: function() {
       var subscribedChannels = this.options.user.subscribedChannels;
       var channel = this.options.items.channel;
-      console.log(subscribedChannels);
+
       if (!subscribedChannels || !subscribedChannels.isDeletingAllowed(channel)) {
         this.$('.action').remove();
       }
