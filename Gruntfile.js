194a2b5032db3097fae57fcf0b9e8974dd633f4c
Lloyd Watkin <lloyd.watkin@surevine.com>
2013-06-14 12:03:32 +0100

    Compress and uglify all CSS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 var requirejs = require('requirejs')
 var config    = requirejs('./config.js')
+var debug     = false
 
 module.exports = function(grunt) {
 
@@ -26,6 +27,11 @@ module.exports = function(grunt) {
   grunt.registerTask('server', ['_server', 'watch']);
   grunt.registerTask('test', ['testacular']);
 
+  // Set debug mode
+  grunt.registerTask('debug', function() {
+	  debug = true
+  })
+  
   // Development Server
   grunt.registerTask('_server', 'Start a custom static web server.', function() {
     var express = require('express');
@@ -35,6 +41,11 @@ module.exports = function(grunt) {
     var app = express();
 
     app.use('/js', express.static(mdir + '/js'));
+    app.use(function(req, res, next) {
+    	if (false == debug) return next()
+    	if ('/css/style.min.css' != req.url) return next()
+    	res.redirect('/css/main.css')
+    })
     app.use('/css', express.static(mdir + '/css'));
     app.use('/img', express.static(mdir + '/img'));
     app.use('/locales', express.static(mdir + '/locales'));
@@ -55,4 +66,19 @@ module.exports = function(grunt) {
     console.log('Listening on port ' + port + '...');
   });
 
+  // Build server
+  grunt.registerTask('build', 'Build the compressed javascript and CSS files', function() {
+	var requirejs = require('requirejs')
+	var cssConfig = {
+	  cssIn: './css/main.css',
+      out: './css/style.min.css',
+	  optimizeCss: 'standard'
+	}
+	requirejs.optimize(cssConfig, function(log) {
+	  console.log(("Application CSS optimisation complete").green);
+	  console.log((log).cyan);
+	}, function(error) {
+	  console.log(("Error optimizing CSS: " + error).red);
+	})
+  })
 }
