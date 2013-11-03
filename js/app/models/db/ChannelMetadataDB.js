57740b1df32c94c8835c67c0551fcf5c56b02943
rodrigods <rodrigodsousa@gmail.com>
2013-11-03 14:33:46 -0300

    Updating DB version

--- js/app/models/db/ChannelMetadataDB.js
+++ js/app/models/db/ChannelMetadataDB.js
@@ -19,7 +19,7 @@ define(function(require) {
     id: 'channel-metadata',
     migrations: [
       {
-        version: '1.0',
+        version: '1.1',
         migrate: function(transaction, next) {
           var store;
 
