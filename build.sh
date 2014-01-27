3a884644ab4fa2eb1e64966dac324ab90e63b685
rodrigods <rodrigodsousa@gmail.com>
2014-01-27 16:27:11 +0100

    Build file to help package creation

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install needed modules
+npm install
+
+# Create config file
+cp config.js.example config.js
+
+# Build
+./node_modules/.bin/grunt build
+
+# Copies target result to target folder
+mkdir -p target
+rsync -av . target/ --exclude=target --exclude=debian \
+   --exclude=.gitignore --exclude=node_modules
+rm -rf target/js/app target/css/main.css
+
