c67809a1fbc322eae73e8347fc84f2f113367522
rodrigods <rodrigodsousa@gmail.com>
2014-02-05 11:23:41 +0100

    Fixing DiscoveryOverlay list

--- js/app/views/overlay/DiscoverOverlay.js
+++ js/app/views/overlay/DiscoverOverlay.js
@@ -33,29 +33,45 @@ define(function(require) {
     },
 
     initialize: function() {
-      if (!localTemplate) localTemplate = l10nBrowser.localiseHTML(template, {});
-      if (!localFooter) localFooter = l10nBrowser.localiseHTML(footer, {});
-      this.model = new DiscoverCollection(api.url('most_active'));
-      this.listenTo(this.model, 'reset', this.render);
-      this.model.fetch({data: {max:10}, reset: true});
+      if (!localTemplate) localTemplate = l10nBrowser.localiseHTML(template, {})
+      if (!localFooter) localFooter = l10nBrowser.localiseHTML(footer, {})
+
+      this.mostActive = new DiscoverCollection(api.url('most_active'))
+      this.popular = new DiscoverCollection(api.url('most_active'))
+
+      var callback = this._triggerRenderCallback([this.mostActive, this.popular])
+      this.listenTo(this.mostActive, 'reset', callback)
+      this.listenTo(this.popular, 'reset', callback)
+
+      this.mostActive.fetch({data: {max: 5, period: 7}, reset: true})
+      this.popular.fetch({data: {max: 5, period: 30}, reset: true})
+    },
+
+    _triggerRenderCallback: function(models) {
+      var self = this
+        , fetched = 0
+      return function() {
+        fetched++
+        if (fetched === models.length) {
+          self.render()
+        }
+      }
     },
 
     render: function() {
-      // FIXME popular must show the channels with biggest number of followers
-      var mostActive = this.model.models.slice(0, 5);
-      var popular = this.model.models.slice(5, 10);
       this.$el.html(_.template(localTemplate, {
-        mostActive: mostActive,
-        popular: popular
-      }));
+        mostActive: this.mostActive.models,
+        popular: this.popular.models
+      }))
+
       // Add footer
-      $('.content').append(_.template(localFooter));
-      avatarFallback(this.$('.avatar'), undefined, 50);
+      $('.content').append(_.template(localFooter))
+      avatarFallback(this.$('.avatar'), undefined, 50)
     },
 
     _redirect: function(event) {
-      var jid = this.$(event.currentTarget).attr('id');
-      Events.trigger('navigate', jid);
+      var jid = this.$(event.currentTarget).attr('id')
+      Events.trigger('navigate', jid)
     }
   });
 
