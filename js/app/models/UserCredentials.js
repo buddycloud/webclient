2643138dabc35afbfae30904337ea72204550755
Abmar Barros <abmargb@gmail.com>
2014-03-20 11:44:44 -0300

    Allows for homeDomain being passed as the 'h' query

--- js/app/models/UserCredentials.js
+++ js/app/models/UserCredentials.js
@@ -69,6 +69,22 @@ define(function(require) {
         $.removeCookie(key);
       }
     },
+    
+    _getQueryVariable: function(variable) {
+      var search = Backbone.history.location.search;
+      if (!search || search == '') {
+        return undefined;
+      }
+      var query = search.substring(1);
+      var vars = query.split("&");
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+          return unescape(pair[1]);
+        }
+      }
+      return undefined;
+    },
 
     isPermanent: function() {
       return Boolean(localStorage.loginPermanent);
@@ -92,7 +108,8 @@ define(function(require) {
       if (username && password) {
         if (username.indexOf('@') == -1) {
           if (config.useURLHostAsDomain) {
-            username += '@' + Backbone.history.location.hostname;
+            username += '@' + (this._getQueryVariable('h') || 
+                Backbone.history.location.hostname);
           } else {
             username += '@' + config.homeDomain;
           }
